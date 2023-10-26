5f99496a11385ff0872c8b1ecf8c57f8278be764
LordOfDeadbush <37131666+LordOfDeadbush@users.noreply.github.com>
2023-10-25 17:18:38 -0700

    added a cursor to the typing effect added a cursor that looks like it's from a terminal to add some CS aesthetic!

--- src/TypingEffect.tsx
+++ src/TypingEffect.tsx
@@ -8,33 +8,49 @@ import "./TypingEffect.css";
 // List of words to be animated through the typing effect.
 const words = ["students", "hackers", "programmers"];
 
+const cursors = {"half": "▄", "full": "█"}
+
+const cursor_blinks = 2;
+
 const TypingEffect: React.FC = () => {
   // State variables for the current word, its substring, and the typing direction.
   const [index, setIndex] = useState(0);
   const [subIndex, setSubIndex] = useState(0);
-  const [reverse, setReverse] = useState(false);
+  const [direction, setDirection] = useState(1);
+  const [cursor, setCursor] = useState(cursors["half"]);
 
   useEffect(() => {
     // Logic for the typing effect (typing, pausing, backspacing).
     if (index === words.length) return;
 
-    if (subIndex === words[index].length + 1 && !reverse) {
-      setReverse(true);
+    if (subIndex === words[index].length && direction === 1) {
+      setDirection(0);
+      for (let i = 1; i <= cursor_blinks; i++) {
+        setTimeout(() => {
+          setCursor(" ");
+        }, (800 * i) - 400)
+        setTimeout(() => {
+          setCursor(cursors["half"]);
+        }, 800 * i)  
+      }
       setTimeout(() => {
-        setReverse(false);
+        setDirection(-1);
+      }, (800 * cursor_blinks) + 400)
+      setTimeout(() => {
+        setDirection(1);
         setIndex((prevIndex) => (prevIndex + 1) % words.length);
         setSubIndex(0);
-      }, 250 * words[index].length);
+      }, 225 * (words[index].length) + (800 * cursor_blinks) + 400);
       return;
     }
 
     const timeout = setTimeout(() => {
-      setSubIndex((prevSubIndex) => prevSubIndex + (reverse ? -1 : 1));
+      setSubIndex((prevSubIndex) => prevSubIndex + (direction));
     }, 200);
 
     // Cleanup function to clear the timeout.
     return () => clearTimeout(timeout);
-  }, [subIndex, index, reverse]);
+  }, [subIndex, index, direction, cursor]);
 
   return (
     <div className="typing-effect">
@@ -42,7 +58,7 @@ const TypingEffect: React.FC = () => {
       <h1>
         where&nbsp;
         <span className="typed-text">
-          {words[index].substring(0, subIndex)}
+          {words[index].substring(0, subIndex) + cursor}
         </span>
       </h1>
       <h1>meet up and hang out</h1>
